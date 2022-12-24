227243b53f55220607c563aab9b9e088a7678ce6
1999keyur <keyurkhakhkhar007@gmail.com>
2022-12-24 23:07:20 +0530

    Added the custom slider at the home page

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Slider.scss";
+import ArrowForwardOutlinedIcon from "@mui/icons-material/ArrowForwardOutlined";
+import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
+import { useState } from "react";
+
+const Slider = () => {
+  const [currentSlide, setcurrentSlide] = useState(0);
+  
+  const data = [
+    "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  ];
+  const numberOfSlides = data.length-1;
+  
+  const nextSlide = () => {
+    setcurrentSlide(currentSlide === numberOfSlides ? 0 : (prev) => prev + 1);
+  };
+
+  const prevSlide = () => {
+    setcurrentSlide(currentSlide === 0 ? numberOfSlides : (prev) => prev - 1);
+  };
+  return (
+    <div className="slider">
+      <div className="container" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
+        <img src={data[0]} alt="" />
+        <img src={data[1]} alt="" />
+        <img src={data[2]} alt="" />
+      </div>
+      <div className="icons">
+        <div className="icon" onClick={prevSlide}>
+          <ArrowBackOutlinedIcon />
+        </div>
+        <div className="icon" onClick={nextSlide}>
+          <ArrowForwardOutlinedIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
