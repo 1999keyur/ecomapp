7cab595dfae102acb73416d7319462b2a720126c
1999keyur <keyurkhakhkhar007@gmail.com>
2022-12-24 23:58:12 +0530

    Changed the slider component to adpat according to the nummber of pictures

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -11,22 +11,24 @@ const Slider = () => {
     "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
     "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
     "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/322207/pexels-photo-322207.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   ];
-  const numberOfSlides = data.length-1;
+  const numberOfSlides = data.length;
   
   const nextSlide = () => {
-    setcurrentSlide(currentSlide === numberOfSlides ? 0 : (prev) => prev + 1);
+    setcurrentSlide(currentSlide === numberOfSlides-1 ? 0 : (prev) => prev + 1);
   };
 
   const prevSlide = () => {
-    setcurrentSlide(currentSlide === 0 ? numberOfSlides : (prev) => prev - 1);
+    setcurrentSlide(currentSlide === 0 ? numberOfSlides-1 : (prev) => prev - 1);
   };
   return (
     <div className="slider">
-      <div className="container" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
+      <div className="container" style={{width:`${numberOfSlides*100}vw`, transform:`translateX(-${currentSlide * 100}vw)`} }>
         <img src={data[0]} alt="" />
         <img src={data[1]} alt="" />
         <img src={data[2]} alt="" />
+        <img src={data[3]} alt="" />
       </div>
       <div className="icons">
         <div className="icon" onClick={prevSlide}>
