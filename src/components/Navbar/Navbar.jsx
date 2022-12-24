94738dcf100a34bb2f5067d7f93d87e2e5e0eb74
1999keyur <keyurkhakhkhar007@gmail.com>
2022-12-24 19:16:45 +0530

    Adding the Header and Footer Componenet

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div className="wrapper">
+          <div className="left">
+            <div className="item">
+              <img src="/img/en.png" alt="" />
+              <KeyboardArrowDownIcon />
+            </div>
+            <div className="item">
+              <span>USD</span>
+              <KeyboardArrowDownIcon />
+            </div>
+            <div className="item">
+              <Link className="link" to="/category/men">
+                Men
+              </Link>
+            </div>
+            <div className="item">
+              <Link className="link" to="/category/women">
+                Women
+              </Link>
+            </div>
+            <div className="item">
+              <Link className="link" to="/category/children">
+                Children
+              </Link>
+            </div>
+          </div>
+          <div className="center">
+            <div className="item">
+              <Link className="link" to="/">
+                ECOMAPP
+              </Link>
+            </div>
+          </div>
+          <div className="right">
+            <div className="item">
+              <Link className="link" to="/">
+                HomePage
+              </Link>
+            </div>
+            <div className="item">
+              <Link className="link" to="/">
+                AboutUs
+              </Link>
+            </div>
+            <div className="item">
+              <Link className="link" to="/">
+                ContactUs
+              </Link>
+            </div>
+
+            <div className="icons">
+              <SearchIcon />
+              <PersonOutlineIcon />
+              <FavoriteBorderIcon />
+              <div className="carticon">
+                <ShoppingCartOutlinedIcon />
+                <span>0</span>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+};
+
+export default Navbar;
